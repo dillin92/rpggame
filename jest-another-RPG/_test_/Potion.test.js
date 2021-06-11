554a6e50b02aef6ec150d125d880ff9045ea502e
dillin92 <dillin970@gmail.com>
2021-06-11 02:44:31 -0400

    player-object, getStats, getInventory

--- jest-another-RPG/_test_/Potion.test.js
+++ jest-another-RPG/_test_/Potion.test.js
@@ -1,4 +1,5 @@
 const { TestWatcher } = require('@jest/core');
+const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
 test('creates a random potion object', () => {
@@ -7,5 +8,6 @@ test('creates a random potion object', () => {
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
+    expect(player.inventory),toEqual(expect.arrayContaining([expect.any(Object)]))
 });
 
